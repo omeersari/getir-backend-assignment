b87de0597ddb68c0b22eb3d5dfa486154013c573
omeersari <omeersari@gmail.com>
2021-12-26 15:45:13 +0300

    app js fix

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const app = express();
-require("dotenv").config();
-
+const errorHandler = require("./middlewares/errorHandler");
 const router  = require("./routes");
 const db = require("./helper/db");
-const errorHandler = require("./middlewares/errorHandler");
+require("dotenv").config();
+
+const app = express();
 app.use(express.json());
 
 db() // db connection
